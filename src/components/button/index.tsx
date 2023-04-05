5c19a5101d95ba8487130abbb15723b5d2d652e5
Usama <102786004+uasghar-smile@users.noreply.github.com>
2023-04-05 21:34:54 +0500

    Implemented share feature for product detail page

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,39 +3,39 @@ import React, { ReactNode } from "react";
 import { theme } from "../../ui";
 
 interface ButtonProps {
-	children: string;
-	onPress: () => void;
-	buttonStyle?: ViewStyle;
+  children: string;
+  onPress: () => void;
+  buttonStyle?: ViewStyle;
 }
 
 export const Button = (props: ButtonProps) => {
-	const { children, onPress, buttonStyle } = props;
-	return (
-		<TouchableOpacity
-			onPress={onPress}
-			style={[styles.buttonContainer, buttonStyle]}
-		>
-			<Text style={styles.title}>{children}</Text>
-		</TouchableOpacity>
-	);
+  const { children, onPress, buttonStyle } = props;
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[styles.buttonContainer, buttonStyle]}
+    >
+      <Text style={styles.title}>{children}</Text>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-	buttonContainer: {
-		height: 38,
-		// width: "100%",
-		justifyContent: "center",
-		alignItems: "center",
-		backgroundColor: theme.colors.primary,
-		borderRadius: 5,
-	},
-	title: {
-		fontSize: theme.fontSize.smallText,
-		lineHeight: 21,
-		fontFamily: theme.fontFamilies.text,
-		letterSpacing: -0.45,
-		textAlign: "center",
-		color: theme.colors.white,
-		textTransform: "uppercase",
-	},
+  buttonContainer: {
+    height: 38,
+    // width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: theme.colors.primary,
+    borderRadius: 5,
+  },
+  title: {
+    fontSize: theme.fontSize.smallText,
+    lineHeight: 21,
+    fontFamily: theme.fontFamilies.text,
+    letterSpacing: -0.45,
+    textAlign: "center",
+    color: theme.colors.white,
+    textTransform: "uppercase",
+  },
 });
